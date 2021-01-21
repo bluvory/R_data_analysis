9bd8832c4b22d6eff06433c91f443e0b663f83a6
bluvory <69047310+bluvory@users.noreply.github.com>
2021-01-21 15:24:33 +0900

    Create CH11.R

---
+++ CH11.R
@@ -0,0 +1,12 @@
+#### 11-1 미국 주별 강력 범죄율 단계 구분도 만들기 ####
+
+
+## -------------------------------------------------------------------- ##
+
+
+install.packages("mapproj")
+install.packages("ggiraphExtra")
+library(ggiraphExtra)
+
+str(USArests)
+
